200ed9861c8b909ceb362d0a97fc10510f5e8b82
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-05-04 13:21:41 -0400

    Implemented timer functionality

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -3,8 +3,53 @@
 angular.module('mainApp')
        .controller('MainController', MainController);
 
-MainController.$inject = ['$scope'];
+MainController.$inject = ['$scope', '$window'];
 
-function MainController($scope){
-  $scope.something = "Hey ya'll!";
+function MainController($scope, $window){
+  var timerInterval;
+  $scope.seconds = "00";
+  $scope.minutes = "0";
+  $scope.startTimer = startTimer;
+
+  function timerCount(){
+    var newMinuteValue;
+    var newSecondsValue;
+
+    if($scope.seconds == null){
+      var secondsValue = 0;
+    } else {
+      var secondsValue = parseInt($scope.seconds);
+    }
+    if($scope.minutes == null){
+      var minutesValue = 0;
+    } else {
+      var minutesValue = parseInt($scope.minutes);
+    }
+
+    if(secondsValue == 59) {
+      $scope.seconds = '00';
+      newMinuteValue = minutesValue + 1;
+      $scope.minutes = newMinuteValue.toString();
+    } else {
+      console.log('oh yeah!')
+      newSecondsValue = (pad(secondsValue + 1));
+      $scope.seconds = newSecondsValue.toString();
+      $scope.minutes = minutesValue.toString();
+    }
+  }
+
+  function pad(num){
+    if(num < 10){
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function startTimer(){
+    if(!timerInterval){
+      timerInterval = $window.setInterval(timerCount, 1000);
+    }
+  }
+
 }
