759d05e8bee0b5d93f9cba1833d6f88b49f0173f
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-05-04 22:32:46 -0400

    Implemented timer and rate calculation functions; started making site look pretty

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -9,32 +9,40 @@ function MainController($scope, $window){
   var timerInterval;
   $scope.seconds = "00";
   $scope.minutes = "0";
+  $scope.displayTimer = false;
+  $scope.displayStart = false;
+  $scope.displayResults = false;
+  $scope.displayInputs = false;
+  $scope.inputTime = inputTime;
   $scope.startTimer = startTimer;
+  $scope.pauseTimer = pauseTimer;
+  $scope.restartTimer = restartTimer;
+  $scope.resetTimer = resetTimer;
+  $scope.calculateRate = calculateRate;
+  $scope.calculateInput = calculateInput;
+
+  $scope.$watch(function(){
+    return $scope.seconds;
+  }, function(){
+    $scope.secondsDisplay = $scope.seconds;
+    $scope.minutesDisplay = $scope.minutes;
+  });
 
   function timerCount(){
-    var newMinuteValue;
+    var newMinutesValue;
     var newSecondsValue;
-
-    if($scope.seconds == null){
-      var secondsValue = 0;
-    } else {
-      var secondsValue = parseInt($scope.seconds);
-    }
-    if($scope.minutes == null){
-      var minutesValue = 0;
-    } else {
-      var minutesValue = parseInt($scope.minutes);
-    }
+    var secondsValue = parseInt($scope.seconds);
+    var minutesValue = parseInt($scope.minutes);
 
     if(secondsValue == 59) {
       $scope.seconds = '00';
-      newMinuteValue = minutesValue + 1;
-      $scope.minutes = newMinuteValue.toString();
+      newMinutesValue = minutesValue + 1;
+      $scope.minutes = newMinutesValue.toString();
     } else {
-      console.log('oh yeah!')
       newSecondsValue = (pad(secondsValue + 1));
       $scope.seconds = newSecondsValue.toString();
       $scope.minutes = minutesValue.toString();
+      console.log($scope.seconds);
     }
   }
 
@@ -47,9 +55,59 @@ function MainController($scope, $window){
   }
 
   function startTimer(){
+    $scope.displayTimer = true;
+    $scope.displayInputs = false;
     if(!timerInterval){
       timerInterval = $window.setInterval(timerCount, 1000);
     }
+  }
+
+  function pauseTimer(){
+    $window.clearInterval(timerInterval);
+    timerInterval = null;
+    $scope.displayStart = true;
+  }
+
+  function restartTimer(){
+    if(!timerInterval){
+      timerInterval = $window.setInterval(timerCount, 1000);
+    }
+    $scope.displayStart = false;
+  }
+
+  function resetTimer(){
+    $scope.seconds = '00';
+    $scope.minutes = '0';
+    $window.clearInterval(timerInterval);
+    timerInterval = null;
+    $scope.displayStart = true;
+  }
+
+  function inputTime(){
+    $scope.displayInputs = true;
+    $scope.displayTimer = false;
+    resetTimer();
+  }
+
+  function calculateRate(){
+    $window.clearInterval(timerInterval);
+    timerInterval = null;
+    var secs = parseInt($scope.seconds);
+    var mins = parseInt($scope.minutes);
+    var totalTime = (mins * 60) + secs;
+
+    $scope.speed = 6 / totalTime;
+    $scope.displayResults = true;
+  }
+
+  function calculateInput(){
+    console.log('made it');
+    var secs = parseInt($scope.inputSecs);
+    var mins = parseInt($scope.inputMins);
+    var totalTime = (mins * 60) + secs;
+
+    $scope.speed = 6 / totalTime;
+    $scope.displayResults = true;
   }
 
 }
